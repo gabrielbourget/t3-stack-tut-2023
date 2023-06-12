5c30b74807c02feaa7ddcbe889bf9d44ecb91d65
Gabriel Bourget <gabrielbourget@Gabriels-MacBook-Pro.local>
2023-06-11 20:16:58 -0400

    rate limiting posts

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,6 +3,16 @@ import type { User } from "@clerk/nextjs/dist/types/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 3 requests per minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  prefix: "ratelimit/chirp",
+});  
 
 
 const filterUserForClient = (user: User) => {
@@ -38,18 +48,25 @@ export const postsRouter = createTRPCRouter({
     });
   }),
 
-  create: privateProcedure.input(z.object({
-    content: z.string().emoji().min(1).max(280),
-  })).mutation(async ({ ctx, input }) => {
-    const authorId = ctx.userId;
+  create: privateProcedure.input(
+    z.object({
+      content: z.string().emoji().min(1).max(280),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
 
-    const post = await ctx.prisma.post.create({
-      data: {
-        authorId,
-        content: input.content,
-      }
-    });
+      const { success } = await ratelimit.limit(authorId);
 
-    return post;
-  })
+      if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        }
+      });
+
+      return post;
+    }
+  )
 });
